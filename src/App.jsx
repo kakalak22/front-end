381207314cd9ecef8ac046c023b39b62556916f9
kakalak11 <tr.gia.bao.94@gmail.com>
2022-12-15 16:47:21 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPost } from "./redux/action";
+import { Header } from "./client/common";
 
 function App() {
-  const { posts, loading } = useSelector((state) => ({ ...state.data }));
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchPost());
-  }, []);
-
   return (
-    <div className="App">
-      {!loading ? (
-        posts.map((post) => {
-          return <p key={post.id}>{post.body}</p>;
-        })
-      ) : (
-        <h3>Loading ...</h3>
-      )}
-    </div>
+    <Routes>
+      <Route path="/">
+        <Route index element={<Header />} />
+      </Route>
+    </Routes>
   );
 }
 
